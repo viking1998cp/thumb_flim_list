0d7131941a9227727db69c245bc164dc2c1f4de8
thangnm <>
2022-01-13 09:22:58 +0700

    clear base

--- lib/base/di.dart
+++ lib/base/di.dart
@@ -1,11 +1,7 @@
 import 'package:base_flutter_framework/repository/get_image_service/get_image_service.dart';
 import 'package:base_flutter_framework/services/network/network.dart';
 import 'package:base_flutter_framework/services/storage_service.dart';
-
-import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-
-import 'package:path_provider/path_provider.dart';
 
 class DependencyInjection {
   static Future<void> init() async {
--- lib/network/bindings/network_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/network_controller.dart';
-
-class NetworkBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<NetworkController>(() => NetworkController());
-  }
-}
